e5415d22d31db3550c320e6e5ed8d15df293eb96
deepak <ddhacks007@gmail.com>
2019-08-17 10:38:24 +0530

    removed adapter class from main activity.

--- app/src/main/java/com/example/honeycomb/MainActivity.java
+++ app/src/main/java/com/example/honeycomb/MainActivity.java
@@ -30,6 +30,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import com.example.honeycomb.ImageCapturing;
+import com.example.honeycomb.MyAdapter;
 
 
 public class MainActivity extends AppCompatActivity  {
